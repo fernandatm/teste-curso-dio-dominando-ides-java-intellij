061eaec663dd42f54a6d4d00db425b42832d9903
fernandatm <fernandabio45@gmail.com>
2022-07-28 14:57:44 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,12 +5,10 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
 
-        Livro livro1 = new Livro("O problema dos 3 corpos", "300");
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
         System.out.println(livro1);
-        
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
@@ -25,9 +23,6 @@ class Livro{
     public Livro(String nome, Integer numPaginas) {
         this.nome = nome;
         this.numPaginas = numPaginas;
-    }
-
-    public Livro(String nome, String numPaginas) {
     }
 
     public String getNome() {
